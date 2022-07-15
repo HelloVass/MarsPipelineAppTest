1bab67ed788e8134da3f562508ff62b94c6ba5c1
chenke <chenke@xd.com>
2022-07-15 17:39:55 +0800

    降级 AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.0.4" apply false
-    id("com.android.library") version "7.0.4" apply false
+    id("com.android.application") version "4.2.2" apply false
+    id("com.android.library") version "4.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.5.31" apply false
 }
 
